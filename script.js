23881c6f5645d20ae9cb68fe24a59be64745bf44
Kaic Leonardo <91261303+kaicrodrigues@users.noreply.github.com>
2022-01-16 15:32:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//Função
+function getDadosEnderecoPorCEP(cep) {
+    //Guardando URL
+    let url = 'https://viacep.com.br/ws/' + cep + '/json/unicode/'
+
+    let xmlHttp = new XMLHttpRequest()
+    xmlHttp.open('GET', url)
+
+    xmlHttp.onreadystatechange = () => {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            let dadosJSONText = xmlHttp.responseText
+            let dadosJSONObj = JSON.parse(dadosJSONText)
+
+            document.getElementById('endereco').value = dadosJSONObj.logradouro
+            document.getElementById('bairro').value = dadosJSONObj.bairro
+            document.getElementById('cidade').value = dadosJSONObj.localidade
+            document.getElementById('uf').value = dadosJSONObj.uf
+
+        }
+    }
+
+    xmlHttp.send()
+}
